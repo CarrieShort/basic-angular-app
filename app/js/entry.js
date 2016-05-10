8c8a90e649035be15d0efe9fa48f48d61cf9eb6b
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-10 10:54:15 -0700

    add delete button

--- app/js/entry.js
+++ app/js/entry.js
@@ -1,3 +1,9 @@
 const angular = require('angular');
 require(__dirname + '/../css/style.css');
-const demoApp = angular.module('demoApp', []);
+const demoApp = angular.module('demoApp', [])
+.controller('Comment', function() {
+  this.msg = '';
+  this.delete = function() {
+      return this.msg = '';
+  };
+});
