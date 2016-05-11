5c759b07caff9cabaf215aec26671f84fe0b77ee
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-11 13:15:09 -0700

    update gulp file to finish build before beginning testing

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 const protractor = require('gulp-protractor').protractor;
 
 gulp.task('webpack:dev', () => {
-  gulp.src('./app/js/entry.js')
+  return gulp.src('./app/js/entry.js')
   .pipe(webpack({
     devtool: 'source-map',
     module: {
@@ -20,8 +20,8 @@ gulp.task('webpack:dev', () => {
   .pipe(gulp.dest('./build'));
 });
 
-gulp.task('static:dev', () => {
-  gulp.src('./app/**/*.html')
+gulp.task('static:dev', ['webpack:dev'], () => {
+  return gulp.src('./app/**/*.html')
   .pipe(gulp.dest('./build'));
 });
 
@@ -43,19 +43,19 @@ gulp.task('lint:test', () => {
   .pipe(eslint.format());
 });
 
-gulp.task('start:server', () => {
+gulp.task('start:server', ['static:dev'], () => {
   exec('node server.js');
   exec('webdriver-manager start');
 });
 
-gulp.task('protractor', () => {
+gulp.task('protractor', ['start:server'], () => {
   return gulp.src('./test/**/*.js')
   .pipe(protractor({
     configFile: 'test/integration/config.js'
   }));
 });
 
-gulp.task('test', ['start:server', 'protractor']);
+gulp.task('test', ['protractor']);
 gulp.task('lint', ['lint:app', 'lint:server']);
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
-gulp.task('default', ['lint', 'build:dev', 'test']);
+gulp.task('default', ['lint', 'test']);
