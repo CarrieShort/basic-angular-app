031345a737be9d21fb5b3c1e29a7daae8cc3bf45
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-13 16:25:45 -0700

    update tests to correct for typo

--- test/integration/db_spec.js
+++ test/integration/db_spec.js
@@ -7,13 +7,13 @@ describe('angular app', () => {
     element(by.model('dinoctrl.newDino.attack')).sendKeys('7');
     element(by.css('.btn-create-dino')).click();
     element(by.css('ul li:last-child p')).getText().then( (text) => {
-      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strenght of 7');
+      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strength of 7');
     });
   });
   it('should get a list of dinosaurs, the last one should be the last one created', () => {
     browser.get('http://localhost:5000');
     element(by.css('ul li:last-child p')).getText().then( (text) => {
-      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strenght of 7');
+      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strength of 7');
     });
   });
   it('should not update on cancel', () => {
@@ -25,7 +25,7 @@ describe('angular app', () => {
     element(by.model('dino.attack')).clear().sendKeys('3');
     element(by.css('ul li:last-child .btn-cancel-dino')).click();
     element(by.css('ul li:last-child p')).getText().then( (text) => {
-      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strenght of 7');
+      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strength of 7');
     });
   });
   it('should update the last dinosaur in the list', () => {
@@ -37,14 +37,14 @@ describe('angular app', () => {
     element(by.model('dino.attack')).clear().sendKeys('3');
     element(by.css('ul li:last-child .btn-update-dino')).click();
     element(by.css('ul li:last-child p')).getText().then( (text) => {
-      expect(text).toEqual('A different guy is a dinosaur that eats mice and has the power of things and an attack strenght of 3');
+      expect(text).toEqual('A different guy is a dinosaur that eats mice and has the power of things and an attack strength of 3');
     });
   });
   it('should delete the last dinosaur', () => {
     browser.get('http://localhost:5000');
     element(by.css('ul li:last-child .btn-remove-dino')).click();
     element(by.css('ul li:last-child p')).getText().then( (text) => {
-      expect(text).not.toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strenght of 7');
+      expect(text).not.toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strength of 7');
     });
   });
 });
