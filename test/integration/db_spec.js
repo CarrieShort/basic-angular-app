662a45bb35e124ab7a516e2263887e0c29fba7d0
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-13 09:20:49 -0700

    e2e test for creating a dinosaur

--- test/integration/db_spec.js
+++ test/integration/db_spec.js
@@ -1,9 +1,13 @@
 describe('angular app', () => {
-  it('should have a 2 way data binding', () => {
+  it('should create a dinosaur', () => {
     browser.get('http://localhost:5000');
-    element(by.model('comment.msg')).sendKeys('hello world');
-    element(by.css('h1')).getText().then( (text) => {
-      expect(text).toEqual('hello world');
+    element(by.model('dinoctrl.newDino.name')).sendKeys('Preachasaurus');
+    element(by.model('dinoctrl.newDino.diet')).sendKeys('heathens');
+    element(by.model('dinoctrl.newDino.specialPower')).sendKeys('religious allegory');
+    element(by.model('dinoctrl.newDino.attack')).sendKeys('7');
+    element(by.css('.btn-create-dino')).click();
+    element(by.css('ul li:last-child p')).getText().then( (text) => {
+      expect(text).toEqual('Preachasaurus is a dinosaur that eats heathens and has the power of religious allegory and an attack strenght of 7');
     });
   });
 });
