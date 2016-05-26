72d58ecdb0211951473617cb38fe9298b92e7316
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-25 22:33:51 -0700

    reformat politician controller and update politician controller test

--- app/js/politicians/controllers/politician_controller.js
+++ app/js/politicians/controllers/politician_controller.js
@@ -1,46 +1,37 @@
 const baseUrl = require('../../config').baseUrl;
 const copy = require('angular').copy;
+
 module.exports = function(app) {
-  app.controller('PoliticianController', ['$http', 'csHandleError', function($http, csHandleError) {
-    var vm = this;
-    vm.politicians = [];
-    vm.errors = [];
-    vm.getAll = () => {
-      $http.get(baseUrl + '/api/politicians')
-        .then((res) => {
-          vm.politicians = res.data;
-        }, csHandleError(vm.errors, 'could not retrieve politicians'));
-    };
-    vm.createPolitician = () => {
-      $http.post(baseUrl + '/api/politicians', vm.newPolitician)
-        .then((res) => {
-          vm.politicians.push(res.data);
-          vm.newPolitician = null;
-        }, csHandleError(vm.errors, 'could not create politician'));
-    };
-    vm.removePolitician = function(politician) {
-      $http.delete(baseUrl + '/api/politicians/' + politician._id)
+  app.controller('PoliticianController', ['csResource', function(Resource) {
+    this.politicians = [];
+    this.errors = [];
+    this.remote = new Resource(this.politicians, this.errors, baseUrl + '/api/politicians');
+
+    this.getAll = this.remote.getAll.bind(this.remote);
+    this.createPolitician = function() {
+      this.remote.save(this.newPolitician)
         .then(() => {
-          vm.politicians.splice(vm.politicians.indexOf(politician), 1);
-        }, csHandleError(vm.errors, 'could not delete politician'));
-    };
-    vm.updatePolitician = function(politician) {
-      $http.put(baseUrl + '/api/politicians/' + politician._id, politician)
+          this.newPolitician = null;
+        });
+    }.bind(this);
+    this.removePolitician = this.remote.remove.bind(this.remote);
+    this.updatePolitician = function(Politician) {
+      this.remote.update(Politician)
         .then(() => {
-          politician.editing = false;
-        }, csHandleError(vm.errors, 'could not update politician'));
+          Politician.editing = false;
+        });
     };
-    vm.startEdit = function(politician) {
-      politician.editing = true;
-      vm.politicianBackup = copy(politician);
-    };
-    vm.cancelEdit = function(politician) {
-      politician.editing = false;
-      for (var key in vm.politicianBackup) {
-        if (vm.politicianBackup.hasOwnProperty(key)) {
-          politician[key] = vm.politicianBackup[key];
+    this.startEdit = function(Politician) {
+      Politician.editing = true;
+      this.PoliticianBackup = copy(Politician);
+    }.bind(this);
+    this.cancelEdit = function(Politician) {
+      Politician.editing = false;
+      for (var key in this.PoliticianBackup) {
+        if (this.PoliticianBackup.hasOwnProperty(key)) {
+          Politician[key] = this.PoliticianBackup[key];
         }
       }
-    };
+    }.bind(this);
   }]);
 };
