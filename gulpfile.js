7da56545af5346b4f536d4991eb81e269a7fc5fc
CarrieShort <carrie.r.phillips@gmail.com>
2016-05-30 16:29:02 -0700

    interpolation

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('sass', () => {
   return gulp.src('./app/sass/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(minifyCss())
+    // .pipe(minifyCss())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./app/css'));
 });
